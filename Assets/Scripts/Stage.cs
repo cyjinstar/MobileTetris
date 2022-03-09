c84702700000f9d1711381c8e37f98f812d49c12
YoungJin <61150078+cyjinstar@users.noreply.github.com>
2022-03-09 20:12:38 +0900

    Update Stage.cs
    Change broken Korean discription.

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 
 public class Stage : MonoBehaviour
 {
-    //�ʿ� �ҽ� �ҷ�����
+    //필요 소스 불러오기
     [Header("Source")]
-    public GameObject tilePrefab; // Ÿ�� ������ ��������
+    public GameObject tilePrefab; // 타일 프리펩 가져오기
     public Transform backgroundNode;
     public Transform boardNode;
     public Transform tetrominoNode;
@@ -18,57 +18,57 @@ public class Stage : MonoBehaviour
     public Text line;
 
     private int indexVal = -1;
-    //inspector��ǥ��Ǵ� ������ ����
+    //inspector에표기되는 변수들 선언
 
 
     [Header("Setting")]
     [Range(4, 40)]
     public int boardWidth = 10;
-    //���� ����
+    //높이 설정
     [Range(5, 20)]
     public int boardHeight = 20;
-    //�������� �ӵ�
+    //떨어지는 속도
     public float fallCycle = 1.0f;
-    //��ġ ����
+    //위치 보정
     public float offset_x = 0f;
     public float offset_y = 0f;
 
     private int halfWidth;
     private int halfHeight;
 
-    private float nextFallTime; // ������ ��Ʈ�ι̳밡 ������ �ð��� ����
+    private float nextFallTime; // 다음에 테트로미노가 떨어질 시간을 저장
 
-    // UI ���� ����
+    // UI 관련 변수
     private int scoreVal = 0;
     private int levelVal = 1;
     private int lineVal;
 
 
-    private void Start() // ������ ���۵ǰ� �� ���� ����
+    private void Start() // start함수는 시작하고 한 번만 실행함
     {
-        // ���� ���۽� text ����
-        lineVal = levelVal * 2;   // �ӽ� ���� ������
+        // 게임 시작시 text 설정
+        lineVal = levelVal * 2;   // 레벨 디자인
         score.text = "" + scoreVal;
         level.text = "" + levelVal;
         line.text = "" + lineVal;
         gameoverPanel.SetActive(false);
-        halfWidth = Mathf.RoundToInt(boardWidth * 0.5f);    // �ʺ��� �߰��� �������ֱ�
-        halfHeight = Mathf.RoundToInt(boardHeight * 0.5f);   // ������ �߰��� �������ֱ�
+        halfWidth = Mathf.RoundToInt(boardWidth * 0.5f);    // 너비의 중간값 설정해주기
+        halfHeight = Mathf.RoundToInt(boardHeight * 0.5f);   // 높이의 중간값 설정해주기
 
-        nextFallTime = Time.time + fallCycle;   // ������ ��Ʈ�ι̳밡 ������ �ð� ����
+        nextFallTime = Time.time + fallCycle;   // 다음에 테트로미노가 떨어질 시간 설정
 
-        CreateBackground(); // ��� �����
-        // ���̸�ŭ �� ��� ������ֱ�
+        CreateBackground(); // 배경 만들기
+        // 높이만큼 행 노드 만들어주기
         for (int i = 0; i < boardHeight; ++i)
         {
-            // ToString�� �̿��Ͽ� ������Ʈ �̸� ����
+            // ToString을 이용하여 오브젝트 이름 설정
             var col = new GameObject((boardHeight - i - 1).ToString());
-            // ��ġ���� -> �� ��ġ�� ����, ���� �߾�
+            // 위치설정 -> 행 위치의 높이, 가로 중앙
             col.transform.position = new Vector3(0, halfHeight - i, 0);
-            // ���� ����� �ڽ����� ����
+            // 보드 노드의 자식으로 설정
             col.transform.parent = boardNode;
         }
-        CreateTetromino(); // ��Ʈ�ι̳� �����
+        CreateTetromino(); // 테트로미노 
     }
 
     public bool longtouch_check = false;
@@ -115,7 +115,7 @@ public class Stage : MonoBehaviour
         restart = true;
     }
 
-    void Update()   // �� �����Ӹ��� ����
+    void Update()   // 매 프레임마다 실행
     {
         if (Application.platform == RuntimePlatform.Android)
         {
@@ -124,7 +124,7 @@ public class Stage : MonoBehaviour
                 Application.Quit();
             }
         }
-        // ���ӿ��� ó��
+        // 게임오버 처리
         if (gameoverPanel.activeSelf)
         {
             if (Input.GetKeyDown("r")||restart)
@@ -133,14 +133,14 @@ public class Stage : MonoBehaviour
                 SceneManager.LoadScene(0);
             }
         }
-        // ���� ó��
+        // 게임 처리
         else
         {
-            //�ʱ�ȭ
-            Vector3 moveDir = Vector3.zero; //�̵� ���� �����
-            bool isRotate = false;  // ȸ�� ���� �����
+            //초기화
+            Vector3 moveDir = Vector3.zero; //이동 여부 저장용
+            bool isRotate = false;  // 회전 여부 저장용
 
-            //�� Ű�� ���� �̵� ���� Ȥ�� ȸ�� ���θ� �������ݴϴ�.
+            //각 키에 따라 이동 여부 혹은 회전 여부를 설정해줍니다.
 
             if (Input.GetKeyDown("a")||leftbtn)
             {
@@ -185,25 +185,25 @@ public class Stage : MonoBehaviour
 
             if (Input.GetKeyDown("r")||restart)
             {
-                // SceneManager�� �̿��Ͽ� ���� ������ϱ�
+                // SceneManager을 이용하여 게임 재시작하기
                 restart = false;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
 
 
-            // �ƹ��� Ű �Է��� ������� Tetromino �������� �ʰ��ϱ�
+            // 아무런 키 입력이 없을경우 Tetromino 움직이지 않게하기
             if (moveDir != Vector3.zero || isRotate)
             {
                 MoveTetromino(moveDir, isRotate);
             }
-            // �Ʒ��� �������� ���� ������ �̵���ŵ�ϴ�.
+            // 아래로 떨어지는 경우는 강제로 이동시킵니다.
             if (Time.time > nextFallTime)
             {
-                nextFallTime = Time.time + fallCycle;   // ���� ������ �ð� �缳��
-                moveDir.y = -1; //  �Ʒ��� �� ĭ �̵�
-                isRotate = false;   // ������ �̵��� ȸ�� ����
+                nextFallTime = Time.time + fallCycle;   // 다음 떨어질 시간 재설정
+                moveDir.y = -1; //  아래로 한 칸 이동
+                isRotate = false;   // 강제로 이동시 회전 없음
 
-                // �ƹ��� Ű �Է��� ������� Tetromino �������� �ʰ��ϱ�
+                // 아무런 키 입력이 없을경우 Tetromino 움직이지 않게하기
                 if (moveDir != Vector3.zero || isRotate)
                 {
                     MoveTetromino(moveDir, isRotate);
@@ -212,35 +212,35 @@ public class Stage : MonoBehaviour
         }
     }
 
-    // �̵��� �������� üũ�� True or False ��ȯ�ϴ� �޼���
+    // 이동이 가능한지 체크후 True or False 반환하는 메서드
     bool MoveTetromino(Vector3 moveDir, bool isRotate)
     {
-        //�̵� or ȸ�� �Ұ��� ���ư��� ���� �� ����
+        //이동 or 회전 불가시 돌아가기 위한 값 저장
         Vector3 oldPos = tetrominoNode.transform.position;
         Quaternion oldRot = tetrominoNode.transform.rotation;
 
-        //�̵� & ȸ�� �ϱ�
+        //이동 & 회전 하기
         tetrominoNode.transform.position += moveDir;
         if (isRotate)
         {
-            //���� ��Ʈ�ι̳� ��忡 90�� ȸ���� ���� ��.
+            //현재 테트로미노 노드에 90도 회전을 더해 줌.
             tetrominoNode.transform.rotation *= Quaternion.Euler(0, 0, 90);
         }
 
-        // �̵� �Ұ��� ���� ��ġ, ȸ�� ���� ���ư���
+        //이동 불가시 이전 위치, 회전 으로 돌아가기
         if (!CanMoveTo(tetrominoNode))
         {
             tetrominoNode.transform.position = oldPos;
             tetrominoNode.transform.rotation = oldRot;
 
-            //�ٴڿ� ��Ҵٴ� �ǹ� = �̵� �Ұ��ϰ� ���� �Ʒ��� �������� �ִ� ��Ȳ
+            //바닥에 닿았다는 의미 = 이동 불가하고 현재 아래로 떨어지고 있는 상황
             if ((int)moveDir.y == -1 && (int)moveDir.x == 0 && isRotate == false)
             {
                 AddToBoard(tetrominoNode);
                 CheckBoardColumn();
                 CreateTetromino();
 
-                //��Ʈ�ι̳� ���� �߰� ���� �̵� ���� Ȯ��
+                //테트로미노 새로 추가 직후 이동 가능 확인
                 if (!CanMoveTo(tetrominoNode))
                 {
                     gameoverPanel.SetActive(true);
@@ -253,82 +253,82 @@ public class Stage : MonoBehaviour
         return true;
     }
 
-    // ��Ʈ�ι̳븦 ���忡 �߰�
+    // 테트로미노를 보드에 추가
     void AddToBoard(Transform root)
     {
         while (root.childCount > 0)
         {
             var node = root.GetChild(0);
 
-            //����Ƽ ��ǥ�迡�� ��Ʈ���� ��ǥ��� ��ȯ
+            //유니티 좌표계에서 테트리스 좌표계로 변환
             int x = Mathf.RoundToInt(node.transform.position.x + halfWidth);
             int y = Mathf.RoundToInt(node.transform.position.y + halfHeight - 1);
 
-            //�θ��� : �� ���(y ��ġ), ������Ʈ �̸� : x ��ġ
+            //부모노드 : 행 노드(y 위치), 오브젝트 이름 : x 위치
             node.parent = boardNode.Find(y.ToString());
             node.name = x.ToString();
         }
     }
 
-    // ���忡 �ϼ��� ���� ������ ����
+    // 보드에 완성된 행이 있으면 삭제
     void CheckBoardColumn()
     {
         bool isCleared = false;
 
-        //�ѹ��� ����� �� ���� Ȯ�ο�
+        //한번에 사라진 행 개수 확인용
         int linecount = 0;
 
-        // �ϼ��� �� == ���� �ڽ� ������ ���� ũ��
+        // 완성된 행 == 행의 자식 갯수가 가로 크기
         foreach (Transform column in boardNode)
         {
             if (column.childCount == boardWidth)
             {
-                //���� ��� �ڽ��� ����
+                //행의 모든 자식을 삭제
                 foreach (Transform tile in column)
                 {
                     Destroy(tile.gameObject);
                 }
-                // ���� ��� �ڽĵ���� ���� ����
+                // 행의 모든 자식들과의 연결 끊기
                 column.DetachChildren();
                 isCleared = true;
-                linecount++; // �ϼ��� �� �ϳ��� linecount ����
+                linecount++; // 완성된 행 하나당 linecount 증가
             }
         }
-        // �ϼ��� ���� ������� ��������
+        // 완성된 행이 있을경우 점수증가
         if (linecount != 0)
         {
             scoreVal += linecount * linecount * 100;
             score.text = "" + scoreVal;
         }
 
-        // �ϼ��� ���� ������� �������� ����
+        // 완성된 행이 있을경우 남은라인 감소
         if (linecount != 0)
         {
             lineVal -= linecount;
-            // ���������� �ʿ� ���� ���ް�� (�ִ� ���� 10���� ����)
+            // 레벨업까지 필요 라인 도달경우 (최대 레벨 10으로 한정)
             if (lineVal <= 0 && levelVal <= 10)
             {
-                levelVal += 1;  // ��������
-                lineVal = levelVal * 2 + lineVal;   // ���� ���� ����
-                fallCycle = 0.08f * (10 - levelVal); // �ӵ� ����
+                levelVal += 1;  // 레벨증가
+                lineVal = levelVal * 2 + lineVal;   // 남은 라인 갱신
+                fallCycle = 0.08f * (10 - levelVal); // 속도 증가
             }
             level.text = "" + levelVal;
             line.text = "" + lineVal;
         }
 
-        // ��� �ִ� ���� �����ϸ� �Ʒ��� ������
+        // 비어 있는 행이 존재하면 아래로 내리기
         if (isCleared)
         {
-            //���� �ٴ� ���� ���� �ʿ䰡 �����Ƿ� index 1 ���� for�� ����
+            //가장 바닥 행은 내릴 필요가 없으므로 index 1 부터 for문 시작
             for (int i = 1; i < boardNode.childCount; ++i)
             {
                 var column = boardNode.Find(i.ToString());
 
-                // �̹� ��� �ִ� ���� ����
+                // 이미 비어 있는 행은 무시
                 if (column.childCount == 0)
                     continue;
 
-                // ���� �� �Ʒ��ʿ� �� ���� �����ϴ��� Ȯ��, �� �ุŭ emptyCol ����
+                // 현재 행 아래쪽에 빈 행이 존재하는지 확인, 빈 행만큼 emptyCol 증가
                 int emptyCol = 0;
                 int j = i - 1;
                 while (j >= 0)
@@ -340,7 +340,7 @@ public class Stage : MonoBehaviour
                     j--;
                 }
 
-                // ���� �� �Ʒ��ʿ� �� �� ����� �Ʒ��� ����
+                // 현재 행 아래쪽에 빈 행 존재시 아래로 내림
                 if (emptyCol > 0)
                 {
                     var targetColumn = boardNode.Find((i - emptyCol).ToString());
@@ -357,23 +357,23 @@ public class Stage : MonoBehaviour
         }
     }
 
-    // �̵� �������� üũ �� True or False ��ȯ�ϴ� �޼���
-    bool CanMoveTo(Transform root)  // tetrominoNode�� �Ű����� root�� ��������
+    // 이동 가능한지 체크 후 True or False 반환하는 메서드
+    bool CanMoveTo(Transform root)  // tetrominoNode를 매개변수 root로 가져오기
     {
-        // tetrominoNode�� �ڽ� Ÿ���� ��� �˻�
+        // tetrominoNode의 자식 타일을 모두 검사
         for (int i = 0; i < root.childCount; ++i)
         {
             var node = root.GetChild(i);
-            // ����Ƽ ��ǥ�迡�� ��Ʈ���� ��ǥ��� ��ȯ
+            // 유니티 좌표계에서 테트리스 좌표계로 변환
             int x = Mathf.RoundToInt(node.transform.position.x + halfWidth);
             int y = Mathf.RoundToInt(node.transform.position.y + halfHeight - 1);
-            // �̵� ������ ��ǥ���� Ȯ�� �� ��ȯ
+            // 이동 가능한 좌표인지 확인 후 반환
             if (x < 1 || x > boardWidth)
                 return false;
             if (y < 0)
                 return false;
 
-            // �̹� �ٸ� Ÿ���� �ִ��� Ȯ��
+            // 이미 다른 타일이 있는지 확인
             var column = boardNode.Find(y.ToString());
 
             if (column != null && column.Find(x.ToString()) != null)
@@ -382,26 +382,26 @@ public class Stage : MonoBehaviour
         return true;
     }
 
-    // Ÿ�� ����
+    // 타일 생성 메소드
     Tile CreateTile(Transform parent, Vector2 position, Color color, int order = 1)
     {
-        var go = Instantiate(tilePrefab); // tilePrefab�� ������ ������Ʈ ����
-        go.transform.parent = parent; // �θ� ����
-        go.transform.localPosition = position; // ��ġ ����
+        var go = Instantiate(tilePrefab); // tilePrefab를 복제한 오브젝트 생성
+        go.transform.parent = parent; // 부모 지정
+        go.transform.localPosition = position; // 위치 지정
 
-        var tile = go.GetComponent<Tile>(); // tilePrefab�� Tile��ũ��Ʈ �ҷ�����
-        tile.color = color; // ���� ����
-        tile.sortingOrder = order; // ���� ����
+        var tile = go.GetComponent<Tile>(); // tilePrefab의 Tile스크립트 불러오기
+        tile.color = color; // 색상 지정
+        tile.sortingOrder = order; // 순서 지정
 
         return tile;
     }
-    // ��� Ÿ���� ����
+    // 배경 타일을 생성
     void CreateBackground()
     {
-        Color color = Color.gray;   // ��� �� ����(���ϴ� ������ ���� ����)
+        Color color = Color.gray;   // 배경 색 설정(원하는 색으로 설정 가능)
 
-        // Ÿ�� ����
-        color.a = 0.6f; // �׵θ��� ���� ���� ������ �ٲٱ�
+        // 타일 보드
+        color.a = 0.6f; // 테두리와 구분 위해 투명도 바꾸기
         for (int x = -halfWidth; x < halfWidth + 1; ++x)
         {
             for (int y = halfHeight; y > -halfHeight; --y)
@@ -410,7 +410,7 @@ public class Stage : MonoBehaviour
             }
         }
 
-        // �¿� �׵θ�
+        // 좌우 테두리
         color.a = 1.0f;
         for (int y = halfHeight; y > -halfHeight; --y)
         {
@@ -418,7 +418,7 @@ public class Stage : MonoBehaviour
             CreateTile(backgroundNode, new Vector2(halfWidth + 1, y), color, 0);
         }
 
-        // �Ʒ� �׵θ�
+        // 아래 테두리
         for (int x = -halfWidth; x <= halfWidth + 1; ++x)
         {
             CreateTile(backgroundNode, new Vector2(x, -halfHeight), color, 0);
@@ -427,7 +427,7 @@ public class Stage : MonoBehaviour
 
     void CreatePreview()
     {
-        // �̹� �ִ� �̸����� �����ϱ�
+        // 이미 있는 미리보기 삭제하기
         foreach (Transform tile in previewNode)
         {
             Destroy(tile.gameObject);
@@ -438,13 +438,13 @@ public class Stage : MonoBehaviour
 
         Color32 color = Color.white;
 
-        // �̸����� ��Ʈ�ι̳� ���� ��ġ
+        // 미리보기 테트로미노 생성 위치
         previewNode.position = new Vector2(-halfWidth - 3, halfHeight - 4);
 
         switch (indexVal)
         {
             case 0: // I
-                color = new Color32(115, 251, 253, 255);    // �ϴû�
+                color = new Color32(115, 251, 253, 255);    // 하늘색
                 CreateTile(previewNode, new Vector2(0f, 2.0f), color);
                 CreateTile(previewNode, new Vector2(0f, 1.0f), color);
                 CreateTile(previewNode, new Vector2(0f, 0.0f), color);
@@ -452,7 +452,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 1: // J
-                color = new Color32(0, 33, 245, 255);    // �Ķ���
+                color = new Color32(0, 33, 245, 255);    // 파란색
                 CreateTile(previewNode, new Vector2(-1f, 0.0f), color);
                 CreateTile(previewNode, new Vector2(0f, 0.0f), color);
                 CreateTile(previewNode, new Vector2(1f, 0.0f), color);
@@ -460,7 +460,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 2: // L
-                color = new Color32(243, 168, 59, 255);    // ��Ȳ��
+                color = new Color32(243, 168, 59, 255);    // 주황색
                 CreateTile(previewNode, new Vector2(-1f, 0.0f), color);
                 CreateTile(previewNode, new Vector2(0f, 0.0f), color);
                 CreateTile(previewNode, new Vector2(1f, 0.0f), color);
@@ -468,7 +468,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 3: // O 
-                color = new Color32(255, 253, 84, 255);    // �����
+                color = new Color32(255, 253, 84, 255);    // 노란색
                 CreateTile(previewNode, new Vector2(0f, 0f), color);
                 CreateTile(previewNode, new Vector2(1f, 0f), color);
                 CreateTile(previewNode, new Vector2(0f, 1f), color);
@@ -476,7 +476,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 4: //  S
-                color = new Color32(117, 250, 76, 255);    // ���
+                color = new Color32(117, 250, 76, 255);    // 녹색
                 CreateTile(previewNode, new Vector2(-1f, -1f), color);
                 CreateTile(previewNode, new Vector2(0f, -1f), color);
                 CreateTile(previewNode, new Vector2(0f, 0f), color);
@@ -484,7 +484,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 5: //  T
-                color = new Color32(155, 47, 246, 255);    // ���ֻ�
+                color = new Color32(155, 47, 246, 255);    // 자주색
                 CreateTile(previewNode, new Vector2(-1f, 0f), color);
                 CreateTile(previewNode, new Vector2(0f, 0f), color);
                 CreateTile(previewNode, new Vector2(1f, 0f), color);
@@ -492,7 +492,7 @@ public class Stage : MonoBehaviour
                 break;
 
             case 6: // Z
-                color = new Color32(235, 51, 35, 255);    // ������
+                color = new Color32(235, 51, 35, 255);    // 빨간색
                 CreateTile(previewNode, new Vector2(-1f, 1f), color);
                 CreateTile(previewNode, new Vector2(0f, 1f), color);
                 CreateTile(previewNode, new Vector2(0f, 0f), color);
@@ -501,27 +501,27 @@ public class Stage : MonoBehaviour
         }
     }
 
-    // ��Ʈ�ι̳� ����
+    // 테트로미노 생성
     void CreateTetromino()
     {
-        //���� ó���� ������ ��Ʈ�ι̳��ΰ��
+        //제일 처음에 나오는 테트로미노인경우
         int index;
         if (indexVal == -1)
         {
-            index = Random.Range(0, 7); // �������� 0~6 ������ �� ����
+            index = Random.Range(0, 7); // 랜덤으로 0~6 사이의 값 생성
         }
-        else index = indexVal;  // Preview�� �� ��������     
+        else index = indexVal;  // Preview의 값 가져오기    
 
         Color32 color = Color.white;
 
-        // ȸ�� ��꿡 ����ϱ� ���� ���ʹϾ� Ŭ����
+        // 회전 계산에 사용하기 위한 쿼터니언 클래스
         tetrominoNode.rotation = Quaternion.identity;
-        // ��Ʈ�ι̳� ���� ��ġ (�߾� ���), �� ���� ����
+        // 테트로미노 생성 위치 (중앙 상단), 값 보정 적용
         tetrominoNode.position = new Vector2(offset_x, halfHeight + offset_y);
 
         switch (index)
         {
-            // ������ ���� ��Ʈ���� ��翡 ����ϰ� ����� ǥ�� (I, J, L ,O, S, T, Z)
+            // 구분을 위해 테트리스 모양에 비슷하게 영어로 표현 (I, J, L ,O, S, T, Z)
 
             case 0: // I
                 color = new Color32(112, 248, 224, 255);    // �ϴû�
